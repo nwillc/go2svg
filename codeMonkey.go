9382c9cdb061edf48bb440b7e9120be74474c111
nwillc <nwillc@gmail.com>
2020-07-16 17:48:10 -0400

    Adding github actions

--- codeMonkey.go
+++ codeMonkey.go
@@ -9,19 +9,19 @@ type codeMonkey struct {
 }
 
 const (
-	blackWhite = "stroke: black; stroke-width: 2; fill: white;"
 	blackBlack = "stroke: black; fill: black;"
+	blackWhite = "stroke: black; stroke-width: 2; fill: white;"
 )
 
-func (codeMonkey *codeMonkey) Ear(rx int, ry int) {
+func (codeMonkey *codeMonkey) ear(rx int, ry int) {
 	codeMonkey.canvas.Circle(rx, ry, 40, blackWhite)
 	codeMonkey.canvas.Circle(rx, ry, 28, blackWhite)
 }
 
-func (codeMonkey *codeMonkey) Eye(rx int, ry int) {
+func (codeMonkey *codeMonkey) eye(rx int, ry int) {
 	codeMonkey.canvas.Circle(rx, ry, 20, blackWhite)
 }
 
-func (codeMonkey *codeMonkey) Nostril(rx int, ry int) {
+func (codeMonkey *codeMonkey) nostril(rx int, ry int) {
 	codeMonkey.canvas.Circle(rx, ry, 4, blackBlack)
 }
--- go2svg.go
+++ go2svg.go
@@ -11,18 +11,18 @@ func main() {
 	// Label
 	codeMonkey.canvas.Text(40, 50, "#CODE", "font-family:monospace;font-size:40px;")
 	// Ears
-	codeMonkey.Ear(100, 100)
-	codeMonkey.Ear(240, 70)
+	codeMonkey.ear(100, 100)
+	codeMonkey.ear(240, 70)
 	// Face
 	codeMonkey.canvas.Circle(180, 140, 80, "stroke: black; stroke-width: 2; fill: #aa450f;")
 	// Eyes
-	codeMonkey.Eye(160, 95)
-	codeMonkey.Eye(160+45, 95-5)
+	codeMonkey.eye(160, 95)
+	codeMonkey.eye(160+45, 95-5)
 	// Muzzle
 	codeMonkey.canvas.Circle(195, 178, 65, blackWhite)
 	// Nostrils
-	codeMonkey.Nostril(178, 138)
-	codeMonkey.Nostril(178+35, 138-5)
+	codeMonkey.nostril(178, 138)
+	codeMonkey.nostril(178+35, 138-5)
 	// Mouth
 	codeMonkey.canvas.Path("M 150 150 C 100,250 305,260 230,140 C 205,190 165,170 150,150 Z", "stroke: black; stroke-width: 2; fill: red;")
 	codeMonkey.canvas.End()
